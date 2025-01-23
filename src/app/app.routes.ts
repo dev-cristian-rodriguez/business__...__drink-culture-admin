7f6a4a6fbcb7cbd33e7acae10c6234ff39e8527d
Cristian <cuentagit1111@gmail.com>
2025-01-22 20:40:13 -0500

    Old reference code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {AppComponent} from "./app.component";
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'home-component/from-app', component: HomeComponentf },
+  { path: 'home-component', component: HomeComponentf },
   { path: 'login-component', component: LoginComponent },
 
 ];
