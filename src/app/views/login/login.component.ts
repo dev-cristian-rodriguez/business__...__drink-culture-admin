13086a03f979413e8b95c2944c386756b64a637e
Cristian <cuentagit1111@gmail.com>
2025-01-25 17:29:49 -0500

    New changes

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,27 +1,53 @@
 import { Component, NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 // Injection is to separate logic from the view
 
 @Component({
   selector: 'app-login',
-  // standalone: true,
-  // imports: [],
-  template: `
-     <main>
-         <h1> Login View </h1>
-         <!-- <li><a routerLink="/login-component" routerLinkActive="active">First Component</a></li> -->
-     </main>
-  `,
-  // templateUrl: './login.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
+  email: FormControl;
+  password: FormControl;
+
+  loading: boolean;
+  additionalMessage: string;
+  status: string;
+
+  constructor() {
+    this.email = new FormControl('');
+    this.password = new FormControl('');
+
+    this.loading = false;
+    this.additionalMessage = '';
+    this.status = '';
+  }
+
+  async submitEvent(event: Event): Promise<void> {
+    event.preventDefault();
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this.additionalMessage = "Login successful";
+      this.status = 'success';
+    }, 4000);
+  }
 
 }
 
 
-@NgModule({
-  declarations: [LoginComponent],
-  exports: [LoginComponent]
-})
-export class LoginComponentModule {}
+// @NgModule({
+//   declarations: [LoginComponent],
+//   exports: [LoginComponent]
+// })
+
+// export class LoginComponentModule {
+// }
