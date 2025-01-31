6895aaf826942b8a31dad3ddac9c76e946ca5e24
Cristian <cuentagit1111@gmail.com>
2025-01-31 12:03:12 -0500

    Not found component added

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,9 +1,13 @@
 import { HomeComponentf } from '../views/home/home.component';
 import { LoginComponent } from "../views/login/login.component";
 
+// Not found component
+import { NotFoundComponent } from '../components/errors/not-found/not-found.component';
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'home-component', component: HomeComponentf },
+  { path: '**', component: NotFoundComponent }
 ];
